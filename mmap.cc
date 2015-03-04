d696d1dc3eaebeb7ffb2ab7f3e8de020efb21c7d
Jan Jongboom <janjongboom@gmail.com>
2015-03-04 19:27:06 +0100

    Formatting

--- mmap.cc
+++ mmap.cc
@@ -8,20 +8,23 @@ using namespace v8;
 v8::Handle<v8::Value> Mmap(const v8::Arguments& args) {
   v8::HandleScope handle_scope;
 
-	if (args.Length() != 5)
-	{
-		return v8::ThrowException(
-			v8::Exception::Error(
-				v8::String::New("mmap requires 5 arguments (length, protection, flags, fd, offset)")));
-	}
+  if (args.Length() != 5)
+  {
+    return v8::ThrowException(
+      v8::Exception::Error(
+        v8::String::New("mmap requires 5 arguments (length, protection, flags, fd, offset)")));
+  }
 
-	const size_t length    = args[0]->ToInteger()->Value();
-	const int protection = args[1]->ToInteger()->Value();
-	const int flags      = args[2]->ToInteger()->Value();
-	const int fd         = args[3]->ToInteger()->Value();
-	const off_t offset   = args[4]->ToInteger()->Value();
-	
-  void* map = mmap(0, length, protection, flags, fd, offset);
+  const size_t length    = args[0]->ToInteger()->Value();
+  const int protection = args[1]->ToInteger()->Value();
+  const int flags      = args[2]->ToInteger()->Value();
+  const int fd         = args[3]->ToInteger()->Value();
+  const int offset   = args[4]->ToInteger()->Value();
+  
+  printf("mmap.cc %lu %d %d %d %d\n", length, protection, flags, fd, offset);
+
+  void* map = mmap(NULL, length, protection, flags, fd, offset);
+  printf("mmap.cc %p\n", map);
   if (map == NULL)
   {
     return v8::ThrowException(node::ErrnoException(errno, "mmap", ""));
@@ -34,16 +37,16 @@ v8::Handle<v8::Value> Msync(const v8::Arguments& args) {
   
   if (args.Length() != 3)
   {
-		return v8::ThrowException(
-			v8::Exception::Error(
-				v8::String::New("msync requires 3 arguments (addr, length, flags)")));
+    return v8::ThrowException(
+      v8::Exception::Error(
+        v8::String::New("msync requires 3 arguments (addr, length, flags)")));
   }
 
   void* map              = (void*)args[0]->ToInteger()->Value();  
-	const size_t length    = args[1]->ToInteger()->Value();
-	const int flags        = args[2]->ToInteger()->Value();
-	
-	return Number::New(msync(map, length, flags));
+  const size_t length    = args[1]->ToInteger()->Value();
+  const int flags        = args[2]->ToInteger()->Value();
+  
+  return Number::New(msync(map, length, flags));
 }
 
 v8::Handle<v8::Value> Munmap(const v8::Arguments& args) {
@@ -51,15 +54,15 @@ v8::Handle<v8::Value> Munmap(const v8::Arguments& args) {
   
   if (args.Length() != 2)
   {
-		return v8::ThrowException(
-			v8::Exception::Error(
-				v8::String::New("munmap requires 2 arguments (addr, length)")));
+    return v8::ThrowException(
+      v8::Exception::Error(
+        v8::String::New("munmap requires 2 arguments (addr, length)")));
   }
   
   void* map              = (void*)args[0]->ToInteger()->Value();  
-	const size_t length    = args[1]->ToInteger()->Value();
-	
-	return Number::New(munmap(map, length));
+  const size_t length    = args[1]->ToInteger()->Value();
+  
+  return Number::New(munmap(map, length));
 }
 
 void init(Handle<Object> exports) {
@@ -67,17 +70,17 @@ void init(Handle<Object> exports) {
   NODE_SET_METHOD(exports, "msync", Msync);
   NODE_SET_METHOD(exports, "munmap", Munmap);
   
-	const v8::PropertyAttribute attribs = (v8::PropertyAttribute) (v8::ReadOnly | v8::DontDelete);
-	exports->Set(v8::String::New("PROT_READ"), v8::Integer::New(PROT_READ), attribs);
-	exports->Set(v8::String::New("PROT_WRITE"), v8::Integer::New(PROT_WRITE), attribs);
-	exports->Set(v8::String::New("PROT_EXEC"), v8::Integer::New(PROT_EXEC), attribs);
-	exports->Set(v8::String::New("PROT_NONE"), v8::Integer::New(PROT_NONE), attribs);
-	exports->Set(v8::String::New("MAP_SHARED"), v8::Integer::New(MAP_SHARED), attribs);
-	exports->Set(v8::String::New("MAP_PRIVATE"), v8::Integer::New(MAP_PRIVATE), attribs);
-	exports->Set(v8::String::New("PAGESIZE"), v8::Integer::New(sysconf(_SC_PAGESIZE)), attribs);
-	exports->Set(v8::String::New("MS_ASYNC"), v8::Integer::New(MS_ASYNC), attribs);
-	exports->Set(v8::String::New("MS_SYNC"), v8::Integer::New(MS_SYNC), attribs);
-	exports->Set(v8::String::New("MS_INVALIDATE"), v8::Integer::New(MS_INVALIDATE), attribs);
+  const v8::PropertyAttribute attribs = (v8::PropertyAttribute) (v8::ReadOnly | v8::DontDelete);
+  exports->Set(v8::String::New("PROT_READ"), v8::Integer::New(PROT_READ), attribs);
+  exports->Set(v8::String::New("PROT_WRITE"), v8::Integer::New(PROT_WRITE), attribs);
+  exports->Set(v8::String::New("PROT_EXEC"), v8::Integer::New(PROT_EXEC), attribs);
+  exports->Set(v8::String::New("PROT_NONE"), v8::Integer::New(PROT_NONE), attribs);
+  exports->Set(v8::String::New("MAP_SHARED"), v8::Integer::New(MAP_SHARED), attribs);
+  exports->Set(v8::String::New("MAP_PRIVATE"), v8::Integer::New(MAP_PRIVATE), attribs);
+  exports->Set(v8::String::New("PAGESIZE"), v8::Integer::New(sysconf(_SC_PAGESIZE)), attribs);
+  exports->Set(v8::String::New("MS_ASYNC"), v8::Integer::New(MS_ASYNC), attribs);
+  exports->Set(v8::String::New("MS_SYNC"), v8::Integer::New(MS_SYNC), attribs);
+  exports->Set(v8::String::New("MS_INVALIDATE"), v8::Integer::New(MS_INVALIDATE), attribs);
 }
 
 NODE_MODULE(mmap, init)
--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 var mmap = require('./build/Release/mmap');
 var fs = require('fs');
 
-var fd = fs.openSync('./package.json', 'r');
+var fd = fs.openSync('./index.js', 'r');
 
-var map = mmap.mmap(4, mmap.PROT_READ, mmap.MAP_SHARED, fd, 0);
+console.log('fd', fd);
+
+var map = mmap.mmap(8, mmap.PROT_READ, mmap.MAP_SHARED, fd, 0);
 console.log('map address is', map);
 console.log('unmap return value is', mmap.munmap(map, 10));
 
