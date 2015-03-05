4a17c7a9f980e656e306ec8dabdf2b146a9f96e1
Jan Jongboom <janjongboom@gmail.com>
2015-03-05 17:45:27 +0100

    randomly pressing keys

--- mmap.cc
+++ mmap.cc
@@ -48,7 +48,7 @@ v8::Handle<v8::Value> Mmap(const v8::Arguments& args) {
   
   printf("mmap.cc mmap succeeded %p\n", map);
   
-  node::Buffer *slowBuffer = node::Buffer::New((char*)blah, length, Map_finalise, (void*)length);
+  node::Buffer *slowBuffer = node::Buffer::New(map, length, Map_finalise, (void*)length);
   v8::Local<v8::Object> globalObj = v8::Context::GetCurrent()->Global();
   v8::Local<v8::Function> bufferConstructor = v8::Local<v8::Function>::Cast(globalObj->Get(buffer_symbol));
   v8::Handle<v8::Value> constructorArgs[3] = { slowBuffer->handle_, args[0], v8::Integer::New(0) };
