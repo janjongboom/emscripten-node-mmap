00a17c46faea54a7af102a234c8f040f7a0c491f
Jan Jongboom <janjongboom@gmail.com>
2015-03-05 15:24:49 +0100

    wip

--- mmap.cc
+++ mmap.cc
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <node_buffer.h>
 #include <v8.h>
+#include <cstdlib>
 
 static v8::Persistent<v8::String> buffer_symbol = NODE_PSYMBOL("Buffer");
 static v8::Persistent<v8::String> ptr_symbol = NODE_PSYMBOL("ptr");
@@ -30,27 +31,32 @@ v8::Handle<v8::Value> Mmap(const v8::Arguments& args) {
   const int flags      = args[2]->ToInteger()->Value();
   const int fd         = args[3]->ToInteger()->Value();
   const int offset   = args[4]->ToInteger()->Value();
-  
+
+  printf("mmap.cc mmap %lu %d %d %d %d\n", length, protection, flags, fd, offset);
   void* map = mmap(NULL, length, protection, flags, fd, offset);
   if (map == NULL)
   {
     return v8::ThrowException(node::ErrnoException(errno, "mmap", ""));
   }
-  
+  printf("mmap.cc mmap succeeded %p\n", map);
+
   node::Buffer *slowBuffer = node::Buffer::New((char*)map, length, Map_finalise, (void*)length);
   v8::Local<v8::Object> globalObj = v8::Context::GetCurrent()->Global();
   v8::Local<v8::Function> bufferConstructor = v8::Local<v8::Function>::Cast(globalObj->Get(buffer_symbol));
   v8::Handle<v8::Value> constructorArgs[3] = { slowBuffer->handle_, args[0], v8::Integer::New(0) };
   v8::Local<v8::Object> actualBuffer = bufferConstructor->NewInstance(3, constructorArgs);
-	
-	actualBuffer->Set(ptr_symbol, Number::New((long)map));
-  
+
+  char ptrStringBuffer[20];
+  sprintf(ptrStringBuffer, "%p", map);
+
+  actualBuffer->Set(ptr_symbol, String::New(ptrStringBuffer));
+
   return actualBuffer;
 }
 
 v8::Handle<v8::Value> Msync(const v8::Arguments& args) {
   v8::HandleScope handle_scope;
-  
+
   if (args.Length() != 3)
   {
     return v8::ThrowException(
@@ -58,32 +64,35 @@ v8::Handle<v8::Value> Msync(const v8::Arguments& args) {
         v8::String::New("msync requires 3 arguments (addr, buffer, flags)")));
   }
 
-  char* map                  = (char*)args[0]->ToInteger()->Value();  
+  unsigned long long mapPtr  = std::strtoull(*v8::String::Utf8Value(args[0]), NULL, 16);
+  char* map                  = (char*)mapPtr;
+  printf("msync %d %d %d!\n", map[12], map[13], map[14]);
   Local<Object> bufferObj    = args[1]->ToObject();
   char*         data         = node::Buffer::Data(bufferObj);
   size_t        len          = node::Buffer::Length(bufferObj);
   const int flags            = args[2]->ToInteger()->Value();
-  
+
   for (size_t i = 0; i < len; i++) {
     map[i] = data[i];
   }
-  
+
   return Number::New(msync(map, len, flags));
 }
 
 v8::Handle<v8::Value> Munmap(const v8::Arguments& args) {
   v8::HandleScope handle_scope;
-  
+
   if (args.Length() != 2)
   {
     return v8::ThrowException(
       v8::Exception::Error(
         v8::String::New("munmap requires 2 arguments (addr, length)")));
   }
-  
-  void* map              = (void*)args[0]->ToInteger()->Value();  
-  const size_t length    = args[1]->ToInteger()->Value();
-  
+
+  unsigned long long mapPtr = std::strtoull(*v8::String::Utf8Value(args[0]), NULL, 16);
+  void* map                 = (void*)mapPtr;
+  const size_t length       = args[1]->ToInteger()->Value();
+
   return Number::New(munmap(map, length));
 }
 
@@ -91,7 +100,7 @@ void init(Handle<Object> exports) {
   NODE_SET_METHOD(exports, "mmap", Mmap);
   NODE_SET_METHOD(exports, "msync", Msync);
   NODE_SET_METHOD(exports, "munmap", Munmap);
-  
+
   const v8::PropertyAttribute attribs = (v8::PropertyAttribute) (v8::ReadOnly | v8::DontDelete);
   exports->Set(v8::String::New("PROT_READ"), v8::Integer::New(PROT_READ), attribs);
   exports->Set(v8::String::New("PROT_WRITE"), v8::Integer::New(PROT_WRITE), attribs);
--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var fd = fs.openSync(__dirname + '/test.js', 'r');
 
 var map = mmap.mmap(8, mmap.PROT_READ, mmap.MAP_SHARED, fd, 0);
 console.log(map);
-console.log('map address is', map.ptr);
-console.log('unmap return value is', mmap.munmap(map, 10));
+console.log('map address is', typeof map.ptr, map.ptr);
+console.log('unmap return value is', mmap.munmap(map.ptr, 8));
 
 fs.closeSync(fd);
