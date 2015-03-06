00aac4a1c8873642cbf6b5fd017f15e11ff8ba81
Jan Jongboom <janjongboom@gmail.com>
2015-03-06 17:44:17 +0100

    Add methods to access the data

--- mmap.cc
+++ mmap.cc
@@ -10,11 +10,9 @@
 
 using namespace v8;
 
-static void Map_finalise(char *data, void*hint)
-{
-	munmap(data, (size_t)hint);
-}
-
+/**
+ * Allocation (mmap, msync, munmap) methods
+ */
 NAN_METHOD(Mmap) {
   NanScope();
 
@@ -23,11 +21,11 @@ NAN_METHOD(Mmap) {
     NanThrowError("mmap requires 5 arguments (length, protection, flags, fd, offset)");
   }
 
-  const size_t length    = args[0]->ToInteger()->Value();
+  const size_t length  = args[0]->ToInteger()->Value();
   const int protection = args[1]->ToInteger()->Value();
   const int flags      = args[2]->ToInteger()->Value();
   const int fd         = args[3]->ToInteger()->Value();
-  const int offset   = args[4]->ToInteger()->Value();
+  const int offset     = args[4]->ToInteger()->Value();
 
   char* map = (char*)mmap(NULL, length, protection, flags, fd, offset);
   if (map == MAP_FAILED)
@@ -35,14 +33,10 @@ NAN_METHOD(Mmap) {
     NanThrowError("Could not initialize map");
   }
   
-  Local<Object> slowBuffer = node::Buffer::New(map, length, Map_finalise, (void*)length);
-  
   char ptrStringBuffer[20];
   sprintf(ptrStringBuffer, "%p", map);
 
-  slowBuffer->ForceSet(NanNew("ptr"), NanNew<v8::String>(ptrStringBuffer));
-
-  NanReturnValue(slowBuffer);
+  NanReturnValue(NanNew<String>(ptrStringBuffer));
 }
 
 NAN_METHOD(Msync) {
@@ -50,20 +44,13 @@ NAN_METHOD(Msync) {
 
   if (args.Length() != 3)
   {
-    NanThrowError("msync requires 3 arguments (addr, buffer, flags)");
+    NanThrowError("msync requires 3 arguments (addr, length, flags)");
   }
 
   unsigned long long mapPtr  = std::strtoull(*v8::String::Utf8Value(args[0]), NULL, 16);
-  char* map                  = (char*)mapPtr;
-  printf("msync %d %d %d!\n", map[12], map[13], map[14]);
-  Local<Object> bufferObj    = args[1]->ToObject();
-  char*         data         = node::Buffer::Data(bufferObj);
-  size_t        len          = node::Buffer::Length(bufferObj);
+  void* map                  = (void*)mapPtr;
+  size_t        len          = args[1]->ToInteger()->Value();
   const int flags            = args[2]->ToInteger()->Value();
-
-  for (size_t i = 0; i < len; i++) {
-    map[i] = data[i];
-  }
 
   NanReturnValue(NanNew<Number>(msync(map, len, flags)));
 }
@@ -83,10 +70,49 @@ NAN_METHOD(Munmap) {
   NanReturnValue(NanNew<Number>(munmap(map, length)));
 }
 
+/**
+ * Reading values from the map
+ */
+NAN_METHOD(geti8) {
+  NanScope();
+  printf("yo geti8 %s\n", *v8::String::Utf8Value(args[0]));
+  char* data = (char*)std::strtoull(*v8::String::Utf8Value(args[0]), NULL, 16);
+  printf("geti8 %p %d\n", data, data[0]);
+  NanReturnValue(NanNew<Number>(data[0]));
+}
+
+NAN_METHOD(geti16) {
+  NanScope();
+  uint16_t* data = (uint16_t*)std::strtoull(*v8::String::Utf8Value(args[0]), NULL, 16);
+  NanReturnValue(NanNew<Number>(data[0]));
+}
+
+NAN_METHOD(geti32) {
+  NanScope();
+  uint32_t* data = (uint32_t*)std::strtoull(*v8::String::Utf8Value(args[0]), NULL, 16);
+  NanReturnValue(NanNew<Number>(data[0]));
+}
+
+  // Module['HEAP8']   = HEAP8   = createNewProxy(RAWHEAP8, 1, 'geti8');
+  // Module['HEAP16']  = HEAP16  = createNewProxy(RAWHEAP16, 2, 'geti16');
+  // Module['HEAP32']  = HEAP32  = createNewProxy(RAWHEAP32, 4, 'geti32');
+  // Module['HEAPU8']  = HEAPU8  = createNewProxy(RAWHEAPU8, 1, 'getu8');
+  // Module['HEAPU16'] = HEAPU16 = createNewProxy(RAWHEAPU16, 2, 'getu16');
+  // Module['HEAPU32'] = HEAPU32 = createNewProxy(RAWHEAPU32, 4, 'getu32');
+  // Module['HEAPF32'] = HEAPF32 = createNewProxy(RAWHEAPF32, 4, 'getf32');
+  // Module['HEAPF64'] = HEAPF64 = createNewProxy(RAWHEAPF64, 8, 'getf64'); 
+
+/**
+ * Module initialization
+ */
 void init(Handle<Object> exports) {
   exports->Set(NanNew<String>("mmap"), NanNew<FunctionTemplate>(Mmap)->GetFunction());
   exports->Set(NanNew<String>("msync"), NanNew<FunctionTemplate>(Msync)->GetFunction());
   exports->Set(NanNew<String>("munmap"), NanNew<FunctionTemplate>(Munmap)->GetFunction());
+  
+  exports->Set(NanNew<String>("geti8"), NanNew<FunctionTemplate>(geti8)->GetFunction());
+  exports->Set(NanNew<String>("geti16"), NanNew<FunctionTemplate>(geti16)->GetFunction());
+  exports->Set(NanNew<String>("geti32"), NanNew<FunctionTemplate>(geti32)->GetFunction());
   
   // const v8::PropertyAttribute attribs = (v8::PropertyAttribute) (v8::ReadOnly | v8::DontDelete);
   // I don't know how to get this f&* attribs on properties now in new native modules
